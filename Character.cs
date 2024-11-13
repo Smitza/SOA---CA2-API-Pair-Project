c5acfdba0eb1f2fabc23e156c3416f75729b48f1
Smitza <seansj02@gmail.com>
2024-11-13 10:18:42 +0000

    Base character class

---
+++ Character.cs
@@ -0,0 +1,13 @@
+﻿namespace SOACA2
+{
+    public class Character
+    {
+        public long id { get; set; }
+        public string name { get; set; }
+        public string description { get; set; }
+        public int health { get; set; }
+        public int speed { get; set; }
+        public string role {  get; set; }
+        
+    }
+}
